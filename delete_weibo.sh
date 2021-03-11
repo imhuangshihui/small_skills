360a50ddb348d1cfb01be747333974ad0006cfc5
imhuangshihui <76728625+imhuangshihui@users.noreply.github.com>
2021-03-11 20:14:25 +0800

    Create delete_weibo.sh

---
+++ delete_weibo.sh
@@ -0,0 +1,57 @@
+# 在浏览器中复制代码，批量删除微博，具体操作方法可搜简书
+
+// ==UserScript==
+
+// @name         Weibored.js
+
+// @namespace    https://vito.sdf.org
+
+// @version      0.2.0
+
+// @description  删除所有微博
+
+// @author       Vito Van
+
+// @match        https://weibo.com/p/*
+
+// @grant        none
+
+// ==/UserScript==
+
+'use strict';
+
+var s = document.createElement('script');
+
+s.setAttribute(
+
+'src',
+
+'https://lib.sinaapp.com/js/jquery/2.0.3/jquery-2.0.3.min.js'
+
+);
+
+s.onload = function() {
+
+setInterval(function() {
+
+if (!$('a[action-type="feed_list_delete"]')) {
+
+$('a.next').click();
+
+} else {
+
+$('a[action-type="feed_list_delete"]')[0].click();
+
+$('a[action-type="ok"]')[0].click();
+
+}
+
+// scroll bottom let auto load
+
+$('html, body').animate({ scrollTop: $(document).height() }, 'slow');
+
+}, 800);
+
+};
+
+document.head.appendChild(s);
